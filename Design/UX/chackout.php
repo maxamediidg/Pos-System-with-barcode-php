80a4aee9f141a153ea5bab3d76b63bd9b0a99c0d
maxamediidg <maxamedeid72@gmail.com>
2024-04-17 00:23:59 +0300

    processing checkout🍕🍕🍕

--- Design/UX/chackout.php
+++ Design/UX/chackout.php
@@ -8,6 +8,59 @@ $products->execute();
 
 $allproducts = $products->fetchAll(PDO::FETCH_OBJ);
 
+if(isset($_SESSION['saleprice'])){
+    $_SESSION['total_price'] = $_SESSION['saleprice'] + 20 ;
+}
+
+$allproducts = $products->fetchAll(PDO::FETCH_OBJ);
+
+
+if(isset($_POST['submit'])) {
+    if(empty($_POST['name']) or empty($_POST['lname'])  or empty($_POST['company_name']) 
+    or empty($_POST['address'])  or empty($_POST['city']) or empty($_POST['country']) or empty($_POST['zip_code'])
+    or empty($_POST['email']) or empty($_POST['phone_number'] or empty($_POST['order_notes']))) {
+
+    echo "<script> alert('one or more inputs are empty');</script>";
+
+    } else{
+
+        $name = $_POST['name'];
+        $lname = $_POST['lname'];
+        $company_name = $_POST['company_name'];
+        $address = $_POST['address'];
+        $city = $_POST['city'];
+        $country = $_POST['country'];
+        $zip_code = $_POST['zip_code'];
+        $email = $_POST['email'];
+        $phone_number = $_POST['phone_number'];
+        $order_notes = $_POST['order_notes'];
+        $saleprice = $_SESSION['total_price'];
+        $user_id = $_SESSION['user_id'];
+
+$insert = $pdo->prepare("insert into orders(name,lname,company_name,address,city,country,zip_code,email,
+phone_number,order_notes,price,user_id)
+        VALUES(:name,:lname,:company_name,:address,:city,:country,:zip_code,:email,:phone_number,:order_notes,:saleprice,:user_id)");
+        
+        $insert->execute([
+            ":name"=> $name,
+            ":lname"=> $lname,
+            ":company_name"=> $company_name,
+            ":address"=> $address,
+            ":city"=> $city,
+            ":country"=> $country,
+            ":zip_code"=> $zip_code,
+            ":email"=> $email,
+            ":phone_number"=> $phone_number,
+            ":order_notes"=> $order_notes,
+            ":saleprice"=> $saleprice,
+            ":user_id"=> $user_id,
+        ]);       
+
+        echo "<script> alert('order has been created');</script>";
+
+    }
+}
+
 ?>
 
         <!-- Single Page Header start -->
@@ -26,63 +79,64 @@ $allproducts = $products->fetchAll(PDO::FETCH_OBJ);
         <div class="container-fluid py-5">
             <div class="container py-5">
                 <h1 class="mb-4">Billing details</h1>
-                <form action="#">
+                <form action="chackout.php" method="post">
                     <div class="row g-5">
                         <div class="col-md-12 col-lg-6 col-xl-7">
                             <div class="row">
                                 <div class="col-md-12 col-lg-6">
                                     <div class="form-item w-100">
                                         <label class="form-label my-3">First Name<sup>*</sup></label>
-                                        <input type="text" class="form-control">
+                                        <input type="text"  name="name" class="form-control">
                                     </div>
                                 </div>
                                 <div class="col-md-12 col-lg-6">
                                     <div class="form-item w-100">
                                         <label class="form-label my-3">Last Name<sup>*</sup></label>
-                                        <input type="text" class="form-control">
+                                        <input type="text" name="lname" class="form-control">
                                     </div>
                                 </div>
                             </div>
                             <div class="form-item">
                                 <label class="form-label my-3">Company Name<sup>*</sup></label>
-                                <input type="text" class="form-control">
+                                <input type="text" name="company_name" class="form-control">
                             </div>
                             <div class="form-item">
                                 <label class="form-label my-3">Address <sup>*</sup></label>
-                                <input type="text" class="form-control" placeholder="House Number Street Name">
+                                <input type="text" class="form-control"  name="address" placeholder="House Number Street Name">
                             </div>
                             <div class="form-item">
                                 <label class="form-label my-3">Town/City<sup>*</sup></label>
-                                <input type="text" class="form-control">
+                                <input type="text" name="city" class="form-control">
                             </div>
                             <div class="form-item">
                                 <label class="form-label my-3">Country<sup>*</sup></label>
-                                <input type="text" class="form-control">
+                                <input type="text"  name="country" class="form-control">
                             </div>
                             <div class="form-item">
                                 <label class="form-label my-3">Postcode/Zip<sup>*</sup></label>
-                                <input type="text" class="form-control">
+                                <input type="text"  name="zip_code" class="form-control">
                             </div>
                             <div class="form-item">
                                 <label class="form-label my-3">Mobile<sup>*</sup></label>
-                                <input type="tel" class="form-control">
+                                <input type="tel"  name="phone_number"  class="form-control">
                             </div>
                             <div class="form-item">
                                 <label class="form-label my-3">Email Address<sup>*</sup></label>
-                                <input type="email" class="form-control">
+                                <input type="email"   name="email" class="form-control">
                             </div>
-                            <div class="form-check my-3">
-                                <input type="checkbox" class="form-check-input" id="Account-1" name="Accounts" value="Accounts">
-                                <label class="form-check-label" for="Account-1">Create an account?</label>
-                            </div>
+                           
                             <hr>
-                            <div class="form-check my-3">
-                                <input class="form-check-input" type="checkbox" id="Address-1" name="Address" value="Address">
-                                <label class="form-check-label" for="Address-1">Ship to a different address?</label>
-                            </div>
-                            <div class="form-item">
-                                <textarea name="text" class="form-control" spellcheck="false" cols="30" rows="11" placeholder="Oreder Notes (Optional)"></textarea>
-                            </div>
+                           
+                            <div class="form-group">
+                                    <textarea class="form-control" name="order_notes" placeholder="Order Notes"></textarea>
+                                </div>
+                            <!-- <div class="form-item mt-1">
+                                <textarea name="text" name="order_notes"  class="form-control" spellcheck="false" cols="30" rows="11" placeholder="Oreder Notes (Optional)"></textarea>
+                            </div> -->
+                            <div class="row g-4 text-center align-items-center justify-content-center pt-4 mt-2">
+                                <button name="submit" type="submit" class="btn border-secondary py-3 px-4 text-uppercase w-100 text-primary">PROCEED TO CHECKOUT</button>
+                            </div>                                                    
+                        </form>
                         </div>
                         <div class="col-md-12 col-lg-6 col-xl-5">
                             <div class="table-responsive">
@@ -188,12 +242,10 @@ $allproducts = $products->fetchAll(PDO::FETCH_OBJ);
                                     </div>
                                 </div>
                             </div>
-                            <div class="row g-4 text-center align-items-center justify-content-center pt-4">
-                                <button type="button" class="btn border-secondary py-3 px-4 text-uppercase w-100 text-primary">Place Order</button>
-                            </div>
+                            
                         </div>
                     </div>
-                </form>
+                
             </div>
         </div>
         <!-- Checkout Page End -->
